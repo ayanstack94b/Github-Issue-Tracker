b02a4248d02e452476af6f7f2dfa76cf50fd075a
unknown <ayondevs.94b@gmail.com>
2026-03-07 15:39:01 +0530

    implement login page with credential validation and dashboard redirect

---
+++ Script/login.js
@@ -0,0 +1,16 @@
+document.getElementById("login-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
+  const usernameValue = username.value;
+  const passwordValue = password.value;
+  console.log("clicked", usernameValue, passwordValue);
+
+  if (usernameValue === "admin" && passwordValue === "admin123") {
+    alert("logged in");
+    window.location.href = "dashboard.html";
+  } else if (usernameValue !== "admin" || passwordValue !== "admin123") {
+    alert("Please use Username:admin & Pass:admin123 to login");
+  }
+});
