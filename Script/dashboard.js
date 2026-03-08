dd0c411aa81badd6ee0b852bf8b24324094a0cc7
unknown <ayondevs.94b@gmail.com>
2026-03-08 13:16:48 +0530

    implement tab filtering, active tab state, and dynamic issue border styling

--- Script/dashboard.js
+++ Script/dashboard.js
@@ -1,8 +1,13 @@
+let allIssues = [];
+
 fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
   .then((res) => res.json())
   .then((data) => {
-    loadAllIssues(data.data);
+    allIssues = data.data;
+    loadAllIssues(allIssues);
+    setActive("tab-all");
   });
+
 function loadAllIssues(cardData) {
   //   console.log(cardData);
 
@@ -15,6 +20,14 @@ function loadAllIssues(cardData) {
   cardData.forEach((card) => {
     // console.log(card.labels);
     const cardDiv = document.createElement("div");
+
+    let borderClass = "";
+
+    if (card.status === "open") {
+      borderClass = "border-top-green";
+    } else if (card.status === "closed") {
+      borderClass = "border-top-purple";
+    }
 
     let statusIcon = "";
 
@@ -34,10 +47,9 @@ function loadAllIssues(cardData) {
   `;
     });
 
-
     cardDiv.innerHTML = `
   
-  <div class="card h-full bg-base-100 shadow-sm border-top-green flex flex-col">
+  <div class="card h-full bg-base-100 shadow-sm ${borderClass} flex flex-col">
 
                     <div class="p-4 space-y-4 flex-1">
                         <div class="flex items-center justify-between">
@@ -68,4 +80,36 @@ function loadAllIssues(cardData) {
   });
 }
 
-// loadAllIssues();
+// active button logic
+
+function setActive(activeId) {
+  document.getElementById("tab-all").classList.remove("active-tab");
+  document.getElementById("tab-open").classList.remove("active-tab");
+  document.getElementById("tab-closed").classList.remove("active-tab");
+
+  document.getElementById(activeId).classList.add("active-tab");
+}
+
+// btn-logics for ALL tabs
+document.getElementById("tab-all").addEventListener("click", function () {
+  setActive("tab-all");
+  loadAllIssues(allIssues);
+});
+
+// btn-logics for Open tabs
+document.getElementById("tab-open").addEventListener("click", function () {
+  setActive("tab-open");
+  const tabOpen = allIssues.filter((openIssues) => {
+    return openIssues.status === "open";
+  });
+  loadAllIssues(tabOpen);
+});
+
+// btn-logics for Closed tabs
+document.getElementById("tab-closed").addEventListener("click", function () {
+  setActive("tab-closed");
+  const tabClosed = allIssues.filter((closedIssues) => {
+    return closedIssues.status === "closed";
+  });
+  loadAllIssues(tabClosed);
+});
